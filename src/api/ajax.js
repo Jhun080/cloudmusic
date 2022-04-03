44910364c483346dc97ec477666591894fe35604
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-04-03 19:29:12 +0800

    使用代理进行跨域

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -7,13 +7,18 @@ import store from '@/store'
 // 1.利用axios对象的方法create，创建一个axios实例
 const requests = axios.create({
   // 配置
-  baseURL: process.env.VUE_APP_BASE_API, // 基本路径前缀（自动匹配生产环境或开发环境）
+  // baseURL: process.env.VUE_APP_BASE_API, // 基本路径前缀（自动匹配生产环境或开发环境）
   // baseURL: 'https://101.42.111.16:9615/api',
-  // baseURL: '/api',
+  baseURL: '/api',
   timeout: 30000, // 请求超时时间
-  withCredentials: true
+  withCredentials: true // 请求携带Cookie
+  // headers: {
+  //   'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+  // }
 
 })
+// requests.defaults.withCredentials = true// 请求携带Cookie
+
 // 请求拦截器：在请求发出前，做一些事情
 requests.interceptors.request.use((config) => {
   // config:配置对象，其中有一个属性，headers请求头
@@ -21,6 +26,7 @@ requests.interceptors.request.use((config) => {
   if (store.state.user.token) {
     config.headers.token = store.state.user.token
   }
+
   return config
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   publicPath: './',
 
-  productionSourceMap: false
+  productionSourceMap: false,
 
   // 代理跨域
-  // devServer: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://www.huecmx.xyz:9416', // 目标服务器
-  //       changOrigin: true, // 允许跨域
-  //       pathRewrite: { // 路径重写
-  //         '^/api': ''
-  //       }
-  //     }
-  //   }
-  // }
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://101.42.111.16:9416', // 目标服务器
+        changOrigin: true, // 允许跨域
+        pathRewrite: { // 路径重写
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
