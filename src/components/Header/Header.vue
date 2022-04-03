89cae9e630f8851592438e033da20b89d50b7633
Jhun <9805325+jhun080@user.noreply.gitee.com>
2022-04-03 19:33:19 +0800

    新增登录功能

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -19,8 +19,9 @@
     <van-popup v-model="showPopup" position="left" :closeable="true">
       <div class="menu-nav">
         <div class="user-nav">
-          <img src="@/assets/user_icon.jpg" class="user-icon" />
-          <div class="user-name">Jhun</div>
+          <img :src="usericon" class="user-icon" />
+          <div class="user-name" v-if="username == '立即登录'" @click="toLogin">{{ username }}</div>
+          <div class="user-name" v-else>{{ username }}</div>
         </div>
         <van-cell-group inset>
           <van-cell :clickable="true" is-link @click="tipMessage('暂未开发此功能')">
@@ -45,8 +46,9 @@
             <template name="title"> <van-icon name="bill-o" style="margin-right: 10px" />我的订单 </template>
           </van-cell>
         </van-cell-group>
-        <van-cell-group inset>
-          <van-cell :clickable="true" @click="tipMessage('暂未开发此功能')">
+        <!-- v-if="username != '立即登录'" -->
+        <van-cell-group inset  @click="logout" >
+          <van-cell :clickable="true" >
             <template name="title">
               <div class="logout">退出登陆 / 关闭</div>
             </template>
@@ -54,11 +56,30 @@
         </van-cell-group>
       </div>
     </van-popup>
+
+    <!-- 底部弹出层 -->
+    <van-popup v-model="showLoginPopup" position="bottom" :safe-area-inset-bottom="true" closeable round class="login-nav" >
+      <div class="title">手机号登录</div>
+      <div class="input-nav">
+        <div class="descript">手机号：</div>
+        <el-input v-model.trim="phoneNum" class="input" placeholder="请输入手机号"></el-input>
+      </div>
+      <div class="input-nav">
+        <div class="descript">验证码：</div>
+        <el-input v-model.trim="checkCode" class="input" placeholder="请输入验证码"></el-input>
+      </div>
+      <div class="input-nav">
+        <el-button type="primary" :disabled=codeDisabled @click="getCheckCode">获取验证码</el-button>
+        <el-button type="primary" style="margin: 0px 10px" @click="loginFunc">登录</el-button>
+      </div>
+
+    </van-popup>
   </div>
 </template>
 
 <script>
-import { Toast } from 'vant'
+import { Toast, Dialog } from 'vant'
+import { setCookie } from '@/utils/token.js'
 
 export default {
   name: 'Header',
@@ -66,16 +87,132 @@ export default {
     return {
       // 展示弹出层
       showPopup: false,
+      // 展示登录弹出层
+      showLoginPopup: false,
       // 查询条件
-      query: ''
+      query: '',
+      // 电话号码
+      phoneNum: '',
+      // 短信验证码
+      checkCode: '',
+      // 发送验证码的可点击状态
+      codeDisabled: false
+    }
+  },
+  computed: {
+    // 用户名
+    username () {
+      const nickname = this.userInfo.nickname
+      if (nickname) {
+        // 如果已登录，可以获取到用户名
+        return nickname
+      } else {
+        // 获取不到用户名，未登录
+        return '立即登录'
+      }
+    },
+    // 用户头像
+    usericon () {
+      const iconUrl = this.userInfo.avatarUrl
+      if (iconUrl) {
+        return iconUrl
+      } else {
+        return require('@/assets/header/unLogin.png')
+      }
+    },
+    // 用户账户信息
+    userInfo () {
+      return this.$store.state.user.userInfo || {}
     }
   },
   mounted () {
-    // this.$API.reqTest().then(res => {
-    //   console.log(res)
-    // })
+
   },
   methods: {
+    // 跳转至登录
+    toLogin () {
+      this.showLoginPopup = true
+    },
+    // 退出登录
+    logout () {
+      Dialog.confirm({
+        title: '确认要退出吗？'
+        // message: '弹窗内容'
+      }).then(async () => {
+        const result = await this.$API.reqLogout()
+        console.log(result)
+        if (result.code === 200) {
+          Toast.success('退出成功')
+        }
+      }).catch(() => {
+        Toast('取消登出操作')
+      })
+    },
+    // 获取验证码
+    getCheckCode () {
+      if (this.phoneNum) {
+        // 已输入手机号
+        const myreg = /^[1][3,4,5,7,8,9][0-9]{9}$/
+        if (!myreg.test(this.phoneNum)) {
+          // 手机号格式不正确
+          Toast.fail('请填写正确的手机号码')
+        } else {
+          // 发送验证码
+          this.$API.reqGetCheckCode(this.phoneNum)
+          Toast.success('验证码已发送')
+          // 禁止短时间内连续发送验证码
+          this.codeDisabled = true
+          setTimeout(() => {
+            this.codeDisabled = false
+          }, 60000)
+        }
+      } else {
+        // 未输入手机号
+        Toast.fail('请输入手机号')
+      }
+    },
+    // 登录功能
+    async loginFunc () {
+      if (this.checkCode) {
+        // 已输入验证码
+        // 验证验证码
+        const result = await this.$API.reqVerifyCode({
+          phoneNum: this.phoneNum,
+          captcha: this.checkCode
+        })
+        if (result.code === 200) {
+          // 登录前先登出
+          await this.$API.reqLogout()
+          // 进行登录
+          const loginResult = await this.$API.reqLoginByCode({
+            phoneNum: this.phoneNum,
+            captcha: this.checkCode
+          })
+          console.log(loginResult)
+          if (loginResult.code === 200) {
+            // 登录成功
+            Toast.success('登录成功')
+            // 获取用户信息
+            this.$store.dispatch('getUserInfo')
+            // 保存用户Cookie
+            setCookie(loginResult.cookie)
+            // 关闭登录弹出层
+            this.showLoginPopup = false
+            this.showPopup = false
+            // 清空输入数据
+            this.phoneNum = ''
+            this.checkCode = ''
+          } else {
+            Toast.fail('登录失败')
+          }
+        } else {
+          Toast.fail('验证码错误')
+        }
+      } else {
+        // 未输入验证码
+        Toast.fail('请输入验证码')
+      }
+    },
     tipMessage (message) {
       Toast(message)
     }
@@ -162,5 +299,32 @@ export default {
       }
     }
   }
+}
+
+.login-nav{
+  width: 100vw;
+  height: 35%;
+
+  .title{
+    text-align: center;
+    font-size: 18px;
+    width: 100%;
+    margin: 20px 0px;
+  }
+
+  .input-nav{
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin: 10px 0px;
+
+    .el-input{
+      width: 60%;
+
+    }
+  }
+
 }
 </style>
